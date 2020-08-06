a55aa16dc824229f4bfd500a15edca7e9ca67e3c
sa-webb <sawebb30@gmail.com>
2020-08-06 17:22:50 -0400

    examples

---
+++ Javascript/algorithms/dfs.js
@@ -0,0 +1,57 @@
+/**
+ * https://fireship.io/courses/javascript/interview-graphs/
+ */
+
+const airports = "PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM".split(" ");
+
+const routes = [
+  ["PHX", "LAX"],
+  ["PHX", "JFK"],
+  ["JFK", "OKC"],
+  ["JFK", "HEL"],
+  ["JFK", "LOS"],
+  ["MEX", "LAX"],
+  ["MEX", "BKK"],
+  ["MEX", "LIM"],
+  ["MEX", "EZE"],
+  ["LIM", "BKK"],
+];
+
+// The graph
+const adjacencyList = new Map();
+
+// Add node
+function addNode(airport) {
+  adjacencyList.set(airport, []);
+}
+
+// Add edge, undirected
+function addEdge(origin, destination) {
+  adjacencyList.get(origin).push(destination);
+  adjacencyList.get(destination).push(origin);
+}
+
+// Create the Graph
+airports.forEach(addNode);
+routes.forEach((route) => addEdge(...route));
+
+function dfs(start, visited = new Set()) {
+  console.log(start);
+
+  visited.add(start);
+
+  const destinations = adjacencyList.get(start);
+
+  for (const destination of destinations) {
+    if (destination === "BKK") {
+      console.log(`DFS found Bangkok`);
+      return;
+    }
+
+    if (!visited.has(destination)) {
+      dfs(destination, visited);
+    }
+  }
+}
+
+dfs("PHX");
