5eccefd042099463c3e8d81e441bde20f2ba9e2f
sa-webb <sawebb30@gmail.com>
2020-08-11 16:49:01 -0400

    final

--- Python/advanced/consecutive_sum_target.py
+++ Python/advanced/consecutive_sum_target.py
@@ -1,19 +1,18 @@
 def sum_pair(l, t):
     l.sort()
-    s = 0
-    e = len(l) - 1
-    a, b = 0, 0
+    s = 0 # start index
+    e = len(l) - 1 # last index
 
+    a, b = 0, 0 # set default return value
     while s < e:
         if (l[s] + l[e] == t):
             a, b = l[s], l[e]
-            return a, b
+            break
         elif (l[s] + l[e] < t):
-            s = + 1
+            s += 1
         else:
             e -= 1
-        return 0
-
+    return a, b
 
 def sum_sequence(l, t):
     sublist = []
@@ -28,19 +27,22 @@ def sum_sequence(l, t):
     return e
 
 
-def main(l,t):
-    check_sum_pair = sum_pair(l,t)
-    check_sum_sequence = sum_sequence(l,t)
-    if(check_sum_pair == 0):
-        print('There was not a pair of numbers that hit the target')
-    elif(check_sum_sequence == [-1,-1]):
-        print('There was not a sequence of numbers that hit the target')
-    else:
-        pass
+def find_sum(l, t):
+    no_pair_sol = (0,0)
+    no_seq_sol = [-1, -1]
+    check_sum_pair = sum_pair(l, t)
+    check_sum_sequence = sum_sequence(l, t)
+    if(check_sum_pair != no_pair_sol):
+        print('check_sum found: ', check_sum_pair)
+    if(check_sum_pair == no_pair_sol):
+        print('sum_pair did not find a solution')
+    if(check_sum_sequence == no_seq_sol):
+        print('sum_sequence did not find a solution')
+    if(check_sum_sequence != no_seq_sol):
+        print('sum_sequence found: ', check_sum_sequence)
 
 
 l1 = [4, 3, 5, 7, 8]
-target = 7
+target = 12
 
-print(sum_pair(l1, target))
-# print(main(l1, target))
+find_sum(l1, target)
