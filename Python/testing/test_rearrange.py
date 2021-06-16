c9327dc01d1abba265cdb55c71d5cca95a062ad6
sa-webb <sawebb30@gmail.com>
2021-06-16 00:19:06 -0400

    feat: additional cases

--- Python/testing/test_rearrange.py
+++ Python/testing/test_rearrange.py
@@ -9,6 +9,21 @@ class TestRearrange(unittest.TestCase):
         testcase = "Lovelace, Ada"
         expected = "Ada Lovelace"
         self.assertEqual(rearrange.rerrange_name(testcase), expected)
+    
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange.rerrange_name(testcase), expected)
+
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange.rerrange_name(testcase), expected)
+    
+    def test_single_name(self):
+        testcase = "Grace"
+        expected = "Grace"
+        self.assertEqual(rearrange.rerrange_name(testcase), expected)
 
 
 unittest.main()
