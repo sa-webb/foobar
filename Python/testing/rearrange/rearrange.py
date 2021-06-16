563f5038ec9dbe79aa71d7a3e5579794b4a7795a
sa-webb <sawebb30@gmail.com>
2021-06-16 00:18:59 -0400

    feat: edge cases

--- Python/testing/rearrange/rearrange.py
+++ Python/testing/rearrange/rearrange.py
@@ -5,4 +5,6 @@ import re
 
 def rerrange_name(name):
     result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+    if result is None:
+        return name
     return "{} {}".format(result[2], result[1])
